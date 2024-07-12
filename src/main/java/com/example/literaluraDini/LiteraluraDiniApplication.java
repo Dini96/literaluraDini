f643a767f722012ae2098f353c09a9bc217faede
Oswaldo Garcia <oswagaru@gmail.com>
2024-07-12 10:16:44 -0600

    Initial commit

---
+++ src/main/java/com/example/literaluraDini/LiteraluraDiniApplication.java
@@ -0,0 +1,13 @@
+package com.example.literaluraDini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraDiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraDiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/literaluraDini/LiteraluraDiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.literaluraDini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraDiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
